e07b009a38457cec15a70f7d13a9e5078e54c899
Rama <ramirocarnicersouble8@gmail.com>
2024-06-11 11:44:47 -0300

    [feat]Mouse Follower working

--- React/mouse-follower/src/App.jsx
+++ React/mouse-follower/src/App.jsx
@@ -2,14 +2,40 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App () {
-  const [button, setButton] = useState(true)
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
   useEffect(() => {
-    console.log('efecto')
-  }, [button])
+    console.log('enabled: ', enabled)
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('HandleMove:', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+      console.log(position)
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
   return (
     <>
-      <button onClick={() => { setButton(!button) }} />
-      <h1>This is an h1 that was rite with VIM</h1>
+      <button onClick={() => { setEnabled(!enabled) }}>{enabled ? 'Desactivar seguimiento' : 'Activar seguimiento'}</button>
+      <div style={{
+        width: '80px',
+        height: '80px',
+        pointerEvents: 'none',
+        borderRadius: '100px',
+        transition: '.05s',
+        position: 'absolute',
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        top: 0,
+        left: 0,
+        backgroundColor: 'red'
+      }}
+      />
     </>
   )
 }
